f745cd6bb14729acffc7deffb08d32e386e42ce8
lmower <lmower@digitalocean.com>
2022-02-24 11:42:15 -0500

    move things and add README.md

---
+++ README.md
@@ -0,0 +1,30 @@
+## gRPC Gateway POC
+
+### What?
+A trivial example of how to run gRPC gateway as a sidecar for an HTTP
+protocol bridge.
+
+### Why?
+Because there are times you want to expose one protocol and use another
+one internally.
+
+### How?
+[grpc-gateway](https://github.com/grpc-ecosystem/grpc-gateway)
+
+### Try it
+In a shell run:
+```
+git clone https://github.com/lowellmower/gateway-poc
+cd gateway-poc && go run main.go
+```
+From another shell session run:
+```
+curl -X POST -k http://localhost:8090/v1/example/echo -d '{"name": " hello"}' | jq .
+```
+You should see:
+```
+{
+  "message": " hello world"
+}
+```
+
--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"log"
 	"net"
+	"net/http"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
-	helloworldpb "github.com/lowellmower/gateway-poc/proto/proto/helloworld"
+	helloworldpb "github.com/lowellmower/gateway-poc/proto/helloworld"
 )
 
 type server struct {
@@ -33,7 +36,36 @@ func main() {
 	s := grpc.NewServer()
 	// Attach the Greeter service to the server
 	helloworldpb.RegisterGreeterServer(s, &server{})
-	// Serve gRPC Server
+	// Serve gRPC server
 	log.Println("Serving gRPC on 0.0.0.0:8080")
-	log.Fatal(s.Serve(lis))
+	go func() {
+		log.Fatalln(s.Serve(lis))
+	}()
+
+	// Create a client connection to the gRPC server we just started
+	// This is where the gRPC-Gateway proxies the requests
+	conn, err := grpc.DialContext(
+		context.Background(),
+		"0.0.0.0:8080",
+		grpc.WithBlock(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatalln("Failed to dial server:", err)
+	}
+
+	gwmux := runtime.NewServeMux()
+	// Register Greeter
+	err = helloworldpb.RegisterGreeterHandler(context.Background(), gwmux, conn)
+	if err != nil {
+		log.Fatalln("Failed to register gateway:", err)
+	}
+
+	gwServer := &http.Server{
+		Addr:    ":8090",
+		Handler: gwmux,
+	}
+
+	log.Println("Serving gRPC-Gateway on http://0.0.0.0:8090")
+	log.Fatalln(gwServer.ListenAndServe())
 }
